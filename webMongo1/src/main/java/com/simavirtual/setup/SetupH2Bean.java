9d547cb13993c397486c1e4c837d9023b1dbf408
naif <nelsonjava@gmail.com>
2015-03-02 15:19:11 -0500

    first commit

---
+++ webMongo1/src/main/java/com/simavirtual/setup/SetupH2Bean.java
@@ -0,0 +1,31 @@
+package com.simavirtual.setup;
+
+import com.simavirtual.model.*;
+
+import java.util.Calendar;
+import java.util.Random;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.logging.Logger;
+
+@Singleton
+@LocalBean
+@Named("setuph2bean")
+public class SetupH2Bean {
+
+    @PersistenceContext(unitName = "picketlink-custom-identity-model-pu")
+    private EntityManager em;
+    private static final Logger log = Logger.getLogger(SetupBean.class.getName());
+
+    public void populateTest() {
+
+        log.info("Please wait while preparing database data H2... ");
+
+        log.info("Database successfully populated H2... ");
+
+    } // end : populateTest Method
+
+}
